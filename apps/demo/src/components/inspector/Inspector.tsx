db53e8682e5b60c987fdd7f660904b3374aa7730
hongqing <zenghongqing@iplusmobot.com>
2025-11-20 11:54:41 +0800

    feat: inspector

--- apps/demo/src/components/inspector/Inspector.tsx
+++ apps/demo/src/components/inspector/Inspector.tsx
@@ -1,15 +1,22 @@
 import { useAppState } from '@/store';
 import type React from 'react';
-import { Typography, Card } from 'antd';
+import { Typography, Card, Form, InputNumber } from 'antd';
 import type {
   InspectorField,
   InspectorSection,
 } from '@didiagu/core/src/primitives/inspector';
+import type { AbstractPrimitive } from '@didiagu/core';
+import { useEffect } from 'react';
 
 const { Text } = Typography;
 
 const Inspector: React.FC = () => {
   const selection = useAppState((state) => state.selection);
+
+  const [form] = Form.useForm();
+  useEffect(() => {
+    form.resetFields();
+  }, [selection, form]);
 
   if (selection.length === 0) {
     return (
@@ -26,33 +33,81 @@ const Inspector: React.FC = () => {
 
   const [firstSelected] = selection;
   const schema = firstSelected.getInspectorFields();
+  const initialValues = schema.reduce((acc, section) => {
+    section.fields.forEach((field) => {
+      acc[field.key] = firstSelected.getParameter(field.key as any);
+    });
+    return acc;
+  }, {} as Record<string, any>);
 
   return (
-    <Card title="Inspector">
-      {schema.map((section) => (
-        <InspectorSection key={section.title} section={section} />
-      ))}
+    <Card size="small" className="shadow-sm h-full">
+      <Form
+        initialValues={initialValues}
+        form={form}
+        variant="filled"
+        colon={false}
+        // layout="inline"
+        labelAlign="right"
+        // onFieldsChange={(fields) => {
+        // console.log('filed', fields);
+        // }}
+        onBlur={() => {
+          console.log('blur');
+          const values = form.getFieldsValue();
+          Object.entries(values).forEach(([key, value]) => {
+            firstSelected.setParameter(key as any, value);
+          });
+        }}
+      >
+        {schema.map((section) => (
+          <InspectorSectionComp
+            key={section.title}
+            section={section}
+            target={firstSelected}
+          />
+        ))}
+      </Form>
     </Card>
   );
 };
 
 interface InspectorFieldProps {
   field: InspectorField;
+  target: AbstractPrimitive;
 }
 
-const InspectorField: React.FC<InspectorFieldProps> = ({ field }) => {
-  return <div>{field.label}</div>;
+const InspectorItem: React.FC<InspectorFieldProps> = ({ field }) => {
+  return (
+    <Form.Item
+      name={field.key}
+      label={field.label}
+      labelCol={{ style: { width: 60 } }}
+      style={{ marginBottom: 6 }}
+    >
+      <InputNumber
+        min={field.min}
+        max={field.max}
+        step={field.step ?? 1}
+        size="small"
+        style={{ width: 70 }}
+      />
+    </Form.Item>
+  );
 };
 
-const InspectorSection: React.FC<{
+const InspectorSectionComp: React.FC<{
   section: InspectorSection;
-}> = ({ section }) => {
+  target: AbstractPrimitive;
+}> = ({ section, target }) => {
   return (
     <div>
       <Typography.Title level={5}>{section.title}</Typography.Title>
-      {section.fields.map((field) => (
-        <InspectorField key={field.key} field={field} />
-      ))}
+      <div className="grid grid-cols-2">
+        {section.fields.map((field) => (
+          <InspectorItem key={field.key} field={field} target={target} />
+        ))}
+      </div>
     </div>
   );
 };
--- packages/core/src/primitives/inspector.ts
+++ packages/core/src/primitives/inspector.ts
@@ -15,22 +15,22 @@ export interface InspectorSection {
 
 export const BASE_INSPECTOR_FIELDS: InspectorSection[] = [
   {
-    title: '位置与大小',
+    title: 'Transform',
     fields: [
       { key: 'x', label: 'X', type: 'number' },
       { key: 'y', label: 'Y', type: 'number' },
-      { key: 'w', label: '宽度', type: 'number', min: 0 },
-      { key: 'h', label: '高度', type: 'number', min: 0 },
+      { key: 'scaleX', label: 'ScaleX', type: 'number', min: 0.1, step: 0.1 },
+      { key: 'scaleY', label: 'ScaleY', type: 'number', min: 0.1, step: 0.1 },
+      { key: 'skewX', label: 'SkewX', type: 'number', step: 1 },
+      { key: 'skewY', label: 'SkewY', type: 'number', step: 1 },
+      { key: 'rotation', label: 'Rotation', type: 'number', step: 1 },
     ],
   },
   {
-    title: '变换',
+    title: 'Dimensions',
     fields: [
-      { key: 'rotation', label: '旋转', type: 'number', step: 1 },
-      { key: 'scaleX', label: '水平缩放', type: 'number', min: 0.1, step: 0.1 },
-      { key: 'scaleY', label: '垂直缩放', type: 'number', min: 0.1, step: 0.1 },
-      { key: 'skewX', label: '水平倾斜', type: 'number', step: 1 },
-      { key: 'skewY', label: '垂直倾斜', type: 'number', step: 1 },
+      { key: 'w', label: 'W', type: 'number', min: 0 },
+      { key: 'h', label: 'H', type: 'number', min: 0 },
     ],
   },
 ];
