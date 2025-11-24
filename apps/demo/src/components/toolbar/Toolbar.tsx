fcf8e6f0d2c7201acf130e34a88ef8521e845827
hongqing <zenghongqing@iplusmobot.com>
2025-11-24 11:40:33 +0800

    fix: 调整toolbar位置

--- apps/demo/src/components/toolbar/Toolbar.tsx
+++ apps/demo/src/components/toolbar/Toolbar.tsx
@@ -109,7 +109,7 @@ const ToolSelector: React.FC = () => {
 
 const Toolbar: React.FC = () => {
   return (
-    <div className="toolbar fixed w-screen bottom-2 flex justify-center">
+    <div className="toolbar fixed w-screen top-2 flex justify-center">
       <div className="w-1/4 h-12 rounded-xl shadow bg-white flex items-stretch">
         <div className="left flex-7 flex items-center justify-center">
           <ToolSelector />
